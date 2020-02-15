4febceae348ded96ef02863586878a881fa6632a
Andrew <andrewyr93@gmail.com>
2020-02-16 00:32:19 +0300

    finish king

--- Bit_Arithmetic/1_king/king.py
+++ Bit_Arithmetic/1_king/king.py
@@ -1,25 +1,23 @@
-l_border = 0xFeFeFeFeFeFeFeFe
-nAB = 0xFcFcFcFcFcFcFcFc
-r_border = 0x7f7f7f7f7f7f7f7f
-nGH = 0x3f3f3f3f3f3f3f3f
+l_boarder = 0xFEFEFEFEFEFEFEFE
+r_boarder = 0x7F7F7F7F7F7F7F7F
+up_boarder = 0x00FFFFFFFFFFFFFF
 
 
 def get_result(x: int):
     start = 1 << x
-    p9 = (start & r_border) << 9
-    p8 = start << 8
-    p7 = (start & l_border) << 7
-    p6 = (start & r_border) << 1
-    p4 = (start & l_border) >> 1
-    p3 = (start & r_border) >> 7
+    p7 = (up_boarder & (start & l_boarder)) << 7
+    p8 = (up_boarder & start) << 8
+    p9 = (up_boarder & (start & r_boarder)) << 9
+    p4 = (start & l_boarder) >> 1
+    p6 = (start & r_boarder) << 1
+    p1 = (start & l_boarder) >> 9
     p2 = start >> 8
-    p1 = (start & l_border) >> 9
+    p3 = (start & r_boarder) >> 7
     movesBits = p9 | p8 | p7 | p6 | p4 | p3 | p2 | p1
     b = movesBits
     q = 0
     while movesBits > 0:
-        if 1 == (movesBits & 1):
-            q += 1
-        movesBits >>= 1
+        q += 1
+        movesBits &= movesBits - 1
     return q, b
 
